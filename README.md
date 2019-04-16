e09843fd3e39ab386bec88496b3a854ee194f188
lvitale <lvitale@hg.com.uy>
2019-04-15 22:56:38 -0300

    Add internationalization example for file About.html. Clean unnecessary pom items. Add Readme content.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # FORTweb
-## Sitio web
+## Sitio en desarrollo
+
+### Deploy
+Para hacer deploy pararse en el directorio raíz del proyecto y 
+ejecutar mvn package. Esto construye un war 
+listo para desplegar en el contenedor. Probado en wildfly 16 y jboss 6.4.  
--- src/main/java/net/lacnic/fortweb/HeaderPanel.java
+++ src/main/java/net/lacnic/fortweb/HeaderPanel.java
@@ -1,11 +1,39 @@
 package net.lacnic.fortweb;
 
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
 import org.apache.wicket.markup.html.panel.Panel;
 
-public class HeaderPanel extends Panel {
-    public HeaderPanel(String id) {
-        super(id);
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
 
-    }
+public class HeaderPanel extends Panel {
+	public HeaderPanel(String id) {
+		super(id);
+		final List<Locale> languageOptions = Arrays.asList(Locale.ENGLISH,
+				new Locale("es", "UY"),
+				new Locale("pt", "BR"));
+
+
+		final ListView<Locale> languages = new ListView<Locale>("locale_item", languageOptions) {
+
+			@Override
+			protected void populateItem(final ListItem<Locale> listItem) {
+				Link<Void> localeLink = new Link<Void>("locale_link") {
+					@Override
+					public void onClick() {
+						getSession().setLocale(listItem.getModelObject());
+					}
+				};
+				localeLink.add(new Label("locale_label", listItem.getModelObject().getDisplayLanguage()));
+				listItem.add(localeLink);
+				add(listItem);
+			}
+		};
+		add(languages);
+
+	}
 }
