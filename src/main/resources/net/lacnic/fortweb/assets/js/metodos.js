8393433b42330e4ad5d146f14f563c95065d0d48
ddelbono <Delbono.14>
2019-04-25 19:36:18 -0300

    Ajustes de maquetado

--- src/main/resources/net/lacnic/fortweb/assets/js/metodos.js
+++ src/main/resources/net/lacnic/fortweb/assets/js/metodos.js
@@ -1,50 +1,50 @@
-$(document).ready(function () {
+$( document ).ready(function() {
 
-	// //  Show nav + overlay-content
-	//    $('.menu-open').click(function() {
-	//        if($(this).hasClass('open')){
-	//            closeNavigation();
-	//        }else{
-	//            openNavigation();
-	//        }
-	//        $(this).toggleClass('open');
-	//    });
+	//  Show nav + overlay-content
+    $('.menu-open').click(function() {
+        if($(this).hasClass('clic')){     
+            closeNavigation();
+        }else{
+            openNavigation();
+        }
+        $(this).toggleClass('clic');
+    });
 
-	//    $('.overlay-content').click(function() {
-	//        closeNavigation();
-	//    });
+    // $('.overlay-content').click(function() {
+    //     closeNavigation();
+    // });
 
-	//    $('.menu-close').click(function() {
-	//    	closeNav();
-	//    });
+    // $('.menu-close').click(function() {
+    // 	closeNav();            
+    // });
 
-	//    function openNavigation(){
-	//    	$('body').addClass('nav-fixed');
-	//    	$('.navigation').fadeIn();
-	//    	$('.js-overlay').fadeIn();
-	//    }
+    function openNavigation(){
+    	// $('body').addClass('nav-fixed');
+    	$('.navigation').fadeIn();
+    	// $('.js-overlay').fadeIn();
+    }
 
-	//    function closeNavigation(){
-	//    	$('body').removeClass('nav-fixed');
-	//    	$('.navigation').fadeOut();
-	//    	$('.js-overlay').fadeOut();
-	//    }
+    function closeNavigation(){
+    	// $('body').removeClass('nav-fixed');
+    	$('.navigation').fadeOut();
+    	// $('.js-overlay').fadeOut();
+    }
 
-	// Header fixed
-	$(function () {
-		var shrinkHeader = 200;
-		$(window).scroll(function () {
-			var scroll = getCurrentScroll();
-			if (scroll >= shrinkHeader) {
-				$('.site-header').addClass('small-header');
-			} else {
-				$('.site-header').removeClass('small-header');
-			}
-		});
-	});
+    // Header fixed    
+    $(function(){        
+        var shrinkHeader = 200;
+        $(window).scroll(function() {
+            var scroll = getCurrentScroll();
+            if ( scroll >= shrinkHeader ) { 
+                $('.site-header').addClass('small-header');
+            }else {
+                $('.site-header').removeClass('small-header');
+            }    
+        }); 
+    });
 
-	function getCurrentScroll() {
-		return window.pageYOffset || document.documentElement.scrollTop;
-	}
+    function getCurrentScroll() {
+        return window.pageYOffset || document.documentElement.scrollTop;
+    }
 
 });
