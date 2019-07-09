0d405aa5488840a7e62578ae28b3c66fc92c3d4e
lvitale <lvitale@hg.com.uy>
2019-07-08 23:18:53 -0300

    Set jsession tracking mode to COOKIE.

--- src/main/java/net/lacnic/fortweb/MyContextLoaderListener.java
+++ src/main/java/net/lacnic/fortweb/MyContextLoaderListener.java
@@ -1,9 +1,14 @@
 package net.lacnic.fortweb;
 
-import javax.servlet.annotation.WebListener;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.SessionTrackingMode;
+import javax.servlet.annotation.WebListener;
+import java.util.EnumSet;
+
 @WebListener
 public class MyContextLoaderListener extends ContextLoaderListener {
     public MyContextLoaderListener() {
@@ -15,5 +20,12 @@ public class MyContextLoaderListener extends ContextLoaderListener {
         context.scan("net.lacnic.fortweb");
         context.refresh();
         return context;
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event) {
+        event.getServletContext()
+                .setSessionTrackingModes(EnumSet.of(SessionTrackingMode.COOKIE));
+        super.contextInitialized(event);
     }
 }
