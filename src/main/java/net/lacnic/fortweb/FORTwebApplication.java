4fdda106561f1d3c1c16e536273d79f8e185c895
Gerardo Pias <gerardo.pias@matesoft.com.uy>
2019-10-28 22:17:03 -0300

    Update validator web page content for new releases anouncements.

--- src/main/java/net/lacnic/fortweb/FORTwebApplication.java
+++ src/main/java/net/lacnic/fortweb/FORTwebApplication.java
@@ -1,18 +1,4 @@
 package net.lacnic.fortweb;
-/*
-import org.apache.wicket.protocol.http.WebApplication;
-public class FORTwebApplication extends WebApplication {
-    public FORTwebApplication() {
-    }
-    *
-     * @see org.apache.wicket.Application#getHomePage()
-
-    @Override
-    public Class getHomePage() {
-        return HomePage.class;
-    }
-}
-*/
 
 import org.apache.wicket.Page;
 import org.apache.wicket.protocol.http.WebApplication;
