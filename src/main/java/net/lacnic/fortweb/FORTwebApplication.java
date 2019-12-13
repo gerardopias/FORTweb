a4a2070d794a40020f118890d406db33a11fba10
lvitale <lvitale@hg.com.uy>
2019-12-12 21:20:32 -0300

    Add fort report section.

--- src/main/java/net/lacnic/fortweb/FORTwebApplication.java
+++ src/main/java/net/lacnic/fortweb/FORTwebApplication.java
@@ -1,6 +1,8 @@
 package net.lacnic.fortweb;
 
 import org.apache.wicket.Page;
+import org.apache.wicket.markup.html.IPackageResourceGuard;
+import org.apache.wicket.markup.html.SecurePackageResourceGuard;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.apache.wicket.spring.injection.annot.SpringComponentInjector;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -20,5 +22,12 @@ public class FORTwebApplication extends WebApplication {
         getComponentInstantiationListeners().add(new SpringComponentInjector(this,
                 WebApplicationContextUtils.getRequiredWebApplicationContext(
                         getServletContext())));
+        IPackageResourceGuard packageResourceGuard = getResourceSettings().getPackageResourceGuard();
+        if (packageResourceGuard instanceof SecurePackageResourceGuard)
+        {
+            SecurePackageResourceGuard guard = (SecurePackageResourceGuard) packageResourceGuard;
+            //allowing pdf files
+            guard.addPattern("+*.pdf");
+        }
     }
 }
